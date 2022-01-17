20935fabd828e9987a4ac2fae53514c9c921809e
Jared Ricioli <jaredbricioli@gmail.com>
2022-01-17 15:56:31 -0300

    adicionamos o array de filmes e a interface de filme

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { IFilme } from '../models/IFilme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -8,6 +9,31 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+    titulo = 'Videos App';
+    listaVideos: IFilme[] = [
+      {
+        nome:'Spider Man - No way home',
+        lancamento: '16/12/2021',
+        duracao: '2h 28m',
+        classificacao: 84,
+        cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fVzXp3NwovUlLe7fvoRynCmBPNc.jpg'
+      },
+      {
+        nome:'Encanto',
+        lancamento: '24/11/2021',
+        duracao: '1h 42m',
+        classificacao: 74,
+        cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/4j0PNHkMr5ax3IA8tjtxcmPU3QT.jpg'
+      },
+      {
+        nome:'Alerta Vermelho',
+        lancamento: '05/11/2021',
+        duracao: '1h 57m',
+        classificacao: 68,
+        cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/pe17f8VDfzbvbHSAKAlcORtBHmW.jpg'
+      }
+    ];
 
   constructor(public alertController: AlertController, public toastController: ToastController) { }
 
